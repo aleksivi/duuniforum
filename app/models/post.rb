479aec7de1b360062152094d0092d4cfd9187526
Aleksi <alex2209@luukku.com>
2011-05-16 14:47:22 +0300

    Changed post-models and makes dropdown values able to show countries names

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
 belongs_to :industry
  belongs_to :focusing_section
-validates_presence_of  :industry_id, :focusing_section_id
+belongs_to :country
+
+validates_presence_of  :industry_id, :focusing_section_id, :country_id
 end
